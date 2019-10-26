56d184c4978b8662d0a70b20533fa2b724b02da8
jacobguinther <jacobgguinther@gmail.com>
2019-10-26 09:40:36 -0600

    make hamburger toggle nav menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("hello")
 const menu = document.querySelector(".burger-menu");
 const menuBars = document.querySelectorAll(".menu-bar");
+const hamburgerContainer = document.querySelector(".hamburger-container");
 
 menu.addEventListener("click", () => {
 	menuBars.forEach(bar => bar.classList.remove("no-animation"));
@@ -10,3 +11,7 @@ menu.addEventListener("click", () => {
 		menu.classList.add("active");
 	}
 });
+hamburgerContainer.addEventListener("click", () => {
+	document.querySelector(".menu").classList.toggle("menu-visible");
+	console.log("You clicked hamburgerContainer")
+})
