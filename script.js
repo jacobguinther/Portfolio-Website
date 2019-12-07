951a2d8c68dcf89fa6f0a23d4944017bcbb2f9ba
jacobguinther <jacobgguinther@gmail.com>
2019-12-07 10:12:30 -0700

    Add picture to about page and brought in aos library

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-console.log("hello")
-const menu = document.querySelector(".burger-menu");
-const menuBars = document.querySelectorAll(".menu-bar");
-const hamburgerContainer = document.querySelector(".hamburger-container");
 
-menu.addEventListener("click", () => {
-	menuBars.forEach(bar => bar.classList.remove("no-animation"));
-	if (menu.classList.contains("active")) {
-		menu.classList.remove("active");
-	} else {
-		menu.classList.add("active");
-	}
+const logo = document.getElementById('logo');
+const menu = document.querySelector('.burger-menu');
+const menuBars = document.querySelectorAll('.menu-bar');
+const hamburgerContainer = document.querySelector('.hamburger-container');
+
+menu.addEventListener('click', () => {
+  menuBars.forEach((bar) => bar.classList.remove('no-animation'));
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active');
+  } else {
+    menu.classList.add('active');
+  }
 });
-hamburgerContainer.addEventListener("click", () => {
-	document.querySelector(".menu").classList.toggle("menu-visible");
-})
+hamburgerContainer.addEventListener('click', () => {
+  document.querySelector('.menu').classList.toggle('menu-visible');
+});
+
+logo.addEventListener('mouseenter', e => {
+	console.log("ENTER")
+	if(!logo.classList.contains('rotate')){
+		logo.classList.add('rotate');
+		setTimeout(()=>logo.classList.remove('rotate'), 1500);
+	}else{console.log("NOOOO")}
+
+});
