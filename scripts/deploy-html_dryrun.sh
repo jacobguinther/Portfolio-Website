0d554aa7c830c24c1c932a15dcebf88bb1fccac8
jacobguinther <jacobgguinther@gmail.com>
2020-04-30 07:53:57 -0600

    add google analytics

--- scripts/deploy-html_dryrun.sh
+++ scripts/deploy-html_dryrun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 aws s3 sync ./html_js_trimmed/ s3://jacobguinther.com \
-				--exclude "*.js" \
-				--metadata '{"Content-Type":"text/html"}' \
-        --dryrun
+	--exclude "*.js" \
+	--content-type 'text/html' \
+	--metadata-directive REPLACE \
+	--dryrun
