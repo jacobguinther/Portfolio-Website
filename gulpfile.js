9b0d06439b66b404a231ad325d4653ee3d0554ef
jacobguinther <jacobgguinther@gmail.com>
2020-05-04 14:56:31 -0600

    Change file structure: now includes src folder
    - Gulp now watches src folder for all .ejs and .sass file changes
    - Outputes to root

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-
 const gulp = require('gulp');
+const log = require('fancy-log');
+const ejs = require('gulp-ejs');
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
 
 sass.compiler = require('node-sass');
 
@@ -10,31 +12,39 @@ sass.compiler = require('node-sass');
 //     cb();
 //   }
 
-  gulp.task('default', function (cb) {
-      console.log("it worked")
-      cb();
-  })
+gulp.task('default', function (cb) {
+  console.log('it worked');
+  cb();
+});
+gulp.task('ejs', function () {
+  return gulp
+    .src('./src/views/*.ejs')
+    .pipe(
+      ejs({
+        msg: 'Hello Gulp!',
+      }).on('error', log),
+    )
+    .pipe(
+      rename(function (path) {
+        path.extname = '.html';
+      }),
+    )
+    .pipe(gulp.dest('./'));
+});
 
-//   gulp.task('message', function() {
-//       return console.log('Gulp is running')
-//   });
+gulp.task('sass', function () {
+  return gulp
+    .src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      rename(function (path) {
+        path.basename = 'styles';
+      }),
+    )
+    .pipe(gulp.dest('./css'));
+});
 
-  gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-  });
-   
-//   gulp.task('sass:watch', function () {
-//     gulp.watch('./sass/**/*.scss', ['sass']);
-//   });
-
-gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
-    // gulp.watch('app/js/*.js', gulp.series('scripts'));
-    // gulp.watch('app/img/*', gulp.series('images'));
-  });
-  
-//   exports.watch = watch;
-//   exports.sass = sass
-//   exports.default = defaultTask
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./src/views/**/*.ejs', gulp.series('ejs'));
+});
